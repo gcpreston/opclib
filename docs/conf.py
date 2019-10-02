a58bd7971be829e978f8cd1d01d643a1e45dcf8c
Graham Preston <preston.gr@husky.neu.edu>
2019-10-01 20:31:09 -0400

    Specify master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ release = '0.0.7'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,6 +41,8 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+master_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
